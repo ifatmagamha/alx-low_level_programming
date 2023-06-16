4abbabcb08ef13ca0897c14a1feaff3499c405e7
ifatmagamha <you@example.com>
2023-06-16 09:40:01 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
  * Description: prints Programming is like building a multilingual puzzle
- * * Return: 0
+ * * Return:Always  0 (Success)
  */
 int main ( void )
 {
-	puts("\"Programming is like building a multilingual puzzel
-");
+	puts("\"Programming is like building a multilingual puzzel");
 	return (0);
 }
-
