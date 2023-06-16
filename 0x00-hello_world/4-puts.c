30c0bde7180caa7e7cfda2eedb1a6f45ce19fde4
ifatmagamha <you@example.com>
2023-06-15 17:08:55 -0700

    c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdi.h>
+/* 
+ * main - Entry point 
+ * 
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	puts("8"Programming is like building a multilingual puzzel");
+	return (0);
+}
